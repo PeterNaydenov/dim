a428b25f635fcde3cfb5c1f94a0c308b80b6a102
Peter Naydenov <peter.naydenov@gmail.com>
2025-08-28 09:31:17 +0300

    Test: Init test

---
+++ test/01-general.test.js
@@ -0,0 +1,9 @@
+import { expect, test, describe } from 'vitest'
+import dim  from '../src/main.js'
+
+describe ( 'Dim - DOM Invisible Markers', () => {
+        
+test ( 'should be defined', () => {
+        expect ( dim ).toBeDefined()
+    })
+}) // describe
